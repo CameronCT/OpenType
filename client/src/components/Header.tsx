8832f80e681d158cdade2dc6c29afbb01dfd0cc1
Cameron <CameronCT@users.noreply.github.com>
2020-11-27 22:00:41 -0800

    chore: modified header and footer

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className={"flex"}>
-        <div className={"w-auto mr-auto"}>
+      <div className={"flex flex-wrap justify-center"}>
+        <div className={"w-full text-center lg:w-1/2 lg:text-left"}>
           <div className={"text-3xl text-white uppercase font-bold tracking-wider"}>
             Open
             <span className={"text-3xl uppercase text-indigo-400"}>Type</span>
+          </div>
+        </div>
+        <div className={"w-full text-center lg:w-1/2 lg:text-right my-auto"}>
+          <div className={"text-sm text-gray-300 uppercase font-semibold tracking-wider"}>
+            The free open-source typing script
           </div>
         </div>
       </div>
